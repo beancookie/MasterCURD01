114470236263d715439cbf72750c99f1022923d2
loveluzhong <loveluzhong@outlook.com>
2019-04-22 17:34:45 +0800

    添加签到页面和一些细节调整

---
+++ litemall-wx/pages/ucenter/signin/signin.js
@@ -0,0 +1,108 @@
+var util = require('../../../utils/util.js');
+var api = require('../../../config/api.js');
+
+var app = getApp();
+
+Page({
+   /**
+   * 页面的初始数据
+   */
+  data: {
+    //img_url: config.imgUrl, //图片地址
+
+    //签到模块
+    signNum: 0,  //签到数
+    signState: false, //签到状态
+    min: 1,  //默认值日期第一天1
+    max: 7,  //默认值日期最后一天7
+    be: 0    //默认倍数
+
+
+  },
+
+  //签到
+  bindSignIn(e) {
+    var that = this,
+      num = e.currentTarget.dataset.num;
+    num++
+    wx.showToast({
+      icon: 'success',
+      title: '签到成功',
+    })
+    that.setData({
+      signNum: num,
+      //signState: true
+    })
+
+    var be = e.currentTarget.dataset.be;
+
+    if (num % 7 == 0) {
+      be += 1;
+      that.setData({
+        be: be
+      })
+    }
+
+    if (num == 7 * be + 1) {
+      that.setData({
+        min: 7 * be + 1,
+        max: 7 * be + 7
+      })
+    }
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    // 页面显示
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+})
