76f426915a5bc83197b7db2152d715f537b1d409
loveluzhong <loveluzhong@outlook.com>
2019-04-23 17:22:28 +0800

    积分变化常量

---
+++ litemall-db/src/main/java/org/linlinjava/litemall/db/util/IntegralConstant.java
@@ -0,0 +1,13 @@
+package org.linlinjava.litemall.db.util;
+
+public class IntegralConstant {
+    /**
+     * 签到积分
+     */
+    public static final Short SIGNIN = 0;
+
+    /**
+     * 管理修改积分
+     */
+    public static final Short ADMIN_CHANGE = 1;
+}
