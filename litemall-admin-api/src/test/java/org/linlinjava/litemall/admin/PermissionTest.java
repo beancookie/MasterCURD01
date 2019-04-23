e3860162d103de6947858d96e4ad467d8b1a4ab7
qianrong <1016848178@qq.com>
2019-04-23 18:23:19 +0800

    LitemallSystemMaper.xml

--- litemall-admin-api/src/test/java/org/linlinjava/litemall/admin/PermissionTest.java
+++ litemall-admin-api/src/test/java/org/linlinjava/litemall/admin/PermissionTest.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.linlinjava.litemall.admin.util.PermVo;
 import org.linlinjava.litemall.admin.util.Permission;
 import org.linlinjava.litemall.admin.util.PermissionUtil;
+import org.linlinjava.litemall.db.dao.LitemallSystemMapper;
+import org.linlinjava.litemall.db.domain.LitemallSystem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -21,11 +23,22 @@ public class PermissionTest {
     @Autowired
     private ApplicationContext context;
 
+    @Autowired
+    private LitemallSystemMapper litemallSystemMapper;
+
     @Test
     public void test() {
-        final String basicPackage = "org.linlinjava.litemall.admin";
-        List<Permission> permissionList = PermissionUtil.listPermission(context, basicPackage);
-        List<PermVo> permVoList = PermissionUtil.listPermVo(permissionList);
-        permVoList.stream().forEach(System.out::println);
+
+        LitemallSystem litemallSystem=new LitemallSystem();
+        litemallSystem.setKeyName("litemall_sign_type");
+        litemallSystem.setKeyValue("0");
+        System.out.println(litemallSystemMapper.updateByKeyName(litemallSystem));
+//        String keyName="litemall_sign_type";
+//        LitemallSystem litemallSystem1= litemallSystemMapper.selectByKeyName(keyName);
+//        System.out.println(litemallSystem1.getKeyValue());
+//        final String basicPackage = "org.linlinjava.litemall.admin";
+//        List<Permission> permissionList = PermissionUtil.listPermission(context, basicPackage);
+//        List<PermVo> permVoList = PermissionUtil.listPermVo(permissionList);
+//        permVoList.stream().forEach(System.out::println);
     }
 }
